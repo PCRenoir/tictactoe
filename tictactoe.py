ec80ff47da88b0923b6c1e8032ac113757a20ac2
PCRenoir <toto3141@live.fr>
2021-04-27 08:43:03 +0200

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -4,7 +4,7 @@ import random as rd
 class Jeu:
     def __init__(self):
         self.plateau = [['.' for i in range(3)] for j in range(3)]
-        self.player = 0  #compteur coups/joueur (en fonction de la parité, j1 ou j2)
+        self.player = 0
 
     def victoire(self, player):
         test_victoire = [[self.plateau[0][0], self.plateau[0][1], self.plateau[0][2]],[self.plateau[1][0], self.plateau[1][1], self.plateau[1][2]], [self.plateau[2][0], self.plateau[2][1], self.plateau[2][2]], [self.plateau[0][0], self.plateau[1][0], self.plateau[2][0]], [self.plateau[0][1], self.plateau[1][1], self.plateau[2][1]], [self.plateau[0][2], self.plateau[1][2], self.plateau[2][2]], [self.plateau[0][0], self.plateau[1][1], self.plateau[2][2]], [self.plateau[2][0], self.plateau[1][1], self.plateau[0][2]]] #test_victoire formé des 3 lignes, 3 colonnes et 2 diagonales
@@ -22,7 +22,7 @@ def affiche_grille():
 
 def clicjeu_h_vs_h(event):
     if game.player % 2 == 0 :
-        couleur = 'red'  #j0 = red -> n° pair
+        couleur = 'red'  #j0 = red ->n° pair
     else:
         couleur = 'blue'  #j1 = blue -> n° impair
     # position du pointeur de la souris
@@ -40,7 +40,7 @@ def clicjeu_h_vs_h(event):
         label = tk.Label(fen, text = 'Égalité')
         label.grid(row = 0, column = 0)
 
-def color():   #retourne la couleur en fonction du compteur joueur
+def color():
     if game.player % 2 == 0 :
         return 'red'
     else:
@@ -56,48 +56,53 @@ def clicjeu_h_vs_pc(event):
             return None
         affiche_cercle(X,Y,couleur)
         if game.victoire(game.player % 2):
-            label = tk.Label(fen, text = 'Joueur humain gagne !')
+            victoire = 'Joueur humain gagne !'
+            label = tk.Label(fen, text = victoire)
             label.grid(row = 0, column = 0)
         game.player += 1
-        a= 0
-        if game.player < 9 and game.victoire((game.player-1) % 2) == False:
-            X = rd.choice(choix)  #X et Y au hasard
-            Y = rd.choice(choix)
-            couleur = color()
-            while game.plateau[Y][X] != '.':
-                X = rd.choice(choix)
-                Y = rd.choice(choix)
-            affiche_cercle(X,Y,couleur)
-            if game.victoire(game.player % 2):
-                label = tk.Label(fen, text = 'Ordinateur gagne !')
-                label.grid(row = 0, column = 0)
-            game.player += 1
+    fen.after(500, computer)    #attend 0.5s avant de faire jouer l'ordi
     if game.player >= 9 and not(game.victoire(game.player % 2) or game.victoire((game.player+1) % 2)):
         label = tk.Label(fen, text = 'Égalité')
         label.grid(row = 0, column = 0)
 
+def computer():
+    choix = [0,1,2]
+    if game.player < 9 and game.victoire((game.player-1) % 2) == False:
+        X = rd.choice(choix)  #X et Y au hasard
+        Y = rd.choice(choix)
+        couleur = color()
+        while game.plateau[Y][X] != '.':
+            X = rd.choice(choix)
+            Y = rd.choice(choix)
+        affiche_cercle(X,Y,couleur)
+        if game.victoire(game.player % 2):
+            victoire = 'Ordinateur gagne !'
+            label = tk.Label(fen, text = victoire)
+            label.grid(row = 0, column = 0)
+        game.player += 1
+
 def cercle(x,y,color):
     dessin.create_oval(x,y,x+80,y+80,width = 2, outline = color)
 
-def affiche_cercle(X,Y,couleur):  #affichage des cercles des joueurs
+def affiche_cercle(X,Y,couleur):
     if game.plateau[Y][X] == '.' :
         cercle(100*X+10,100*Y+10, couleur)
         game.plateau[Y][X] = game.player % 2
     else :
         game.player -= 1
 
-def jeupc(): #lance le jeu humain vs pc
+def jeupc():
     affiche_grille()
     label.destroy()
     dessin.bind('<Button-1>', clicjeu_h_vs_pc)
 
-def jeuhumain():  #lance le jeu humain vs humain
+def jeuhumain():
     affiche_grille()
     label.destroy()
     dessin.bind('<Button-1>', clicjeu_h_vs_h)
 
 
-################## Programme principal ################
+###################### Programme principal ##############
 game = Jeu()
 fen = tk.Tk()
 fen.title('TicTacToe')
@@ -108,7 +113,7 @@ dessin.grid(row = 0, column = 0)
 label = tk.Label(fen, text = 'Choisir le mode de jeu dans le menu Jeu')
 label.grid(row = 0, column = 0)
 
-menubar = tk.Menu(fen)  #definition du menu
+menubar = tk.Menu(fen)
 menu1 = tk.Menu(menubar, tearoff = 0)
 menu1.add_command(label = 'Humain vs Humain', command = jeuhumain)
 menu1.add_command(label = 'Humain vs pc', command = jeupc)
